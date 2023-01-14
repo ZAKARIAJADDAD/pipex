c3de9b9e7a4fd32b946cbdf5520e8457b47c63f2
Zakaria Jaddad <zjaddad@e1r4p10.1337.ma>
2023-01-14 22:33:59 +0100

    push

--- cmds.c
+++ cmds.c
@@ -6,7 +6,7 @@
 /*   By: zjaddad <zjaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 12:11:46 by zjaddad           #+#    #+#             */
-/*   Updated: 2023/01/14 21:02:42 by zjaddad          ###   ########.fr       */
+/*   Updated: 2023/01/14 22:02:44 by zjaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,10 @@ void	path_cm1(char **av, char **env, t_pp pp)
 
 	s_path = path_split(env);
 	pp.p_data.cmd = ft_split(av[2], ' ');
-	printf("cmd1 = %s\n", pp.p_data.cmd[0]);
 	if (av[2][0] == '/')
 	{
 		execve(av[2], pp.p_data.cmd, env);
-		write(2, "cmd not found!", 15);
+		write(2, "command not found!", 19);
 		exit(1);
 	}
 	if (s_path)
@@ -37,7 +36,7 @@ void	path_cm1(char **av, char **env, t_pp pp)
 				execve(pp.p_data.link2, pp.p_data.cmd, env);
 		}
 	}
-	write(2, "cmd not found\n", 15);
+	write(2, "command not found!\n", 20);
 	exit(127);
 }
 
@@ -57,15 +56,14 @@ void	path_cm2(char **av, char **env, t_pp pp)
 	if (s_paths)
 	{
 		i = 0;
-		while (s_paths[i])
+		while (s_paths[i++])
 		{
 			pp.p_data.link1 = ft_strjoin(s_paths[i], "/");
 			pp.p_data.link2 = ft_strjoin(pp.p_data.link1, pp.p_data.cmd[0]);
 			if (access(pp.p_data.link2, X_OK) == 0)
 				execve(pp.p_data.link2, pp.p_data.cmd, env);
-			i++;
 		}
 	}
-	write(2, "cmd not found\n", 15);
+	write(2, "command not found!\n", 20);
 	exit(127);
 }
